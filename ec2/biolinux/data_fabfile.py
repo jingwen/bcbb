03ec5fa017b0551d559bcff39d8a3caa7a9f82a5
JingWen Wang <jingwen.wang@scilifelab.se>
2011-04-06 16:32:16 +0200

    My own changes to work on UPPMAX cluster

--- ec2/biolinux/data_fabfile.py
+++ ec2/biolinux/data_fabfile.py
@@ -11,45 +11,33 @@ Usage:
 """
 import os
 import operator
-import socket
-import glob
 from contextlib import contextmanager
 
-from fabric.main import load_settings
 from fabric.api import *
 from fabric.contrib.files import *
-try:
-    import boto
-except ImportError:
-    boto = None
 
 # -- Host specific setup for various groups of servers.
 
-env.config_dir = os.path.join(os.path.dirname(__file__), "config")
 env.remove_old_genomes = False
 
-def setup_environment():
-    """Setup environment with required data file locations.
-    """
-    _add_defaults()
-    _amazon_ec2_environment()
-
-def _amazon_ec2_environment():
-    """Setup for a ubuntu amazon ec2 share; defaults if not set.
+def amazon_ec2():
+    """Setup for a ubuntu amazon ec2 share.
 
     Need to pass in host and private key file on commandline:
         -H hostname -i private_key_file
     """
-    if not env.has_key("user") or not env.user:
-        env.user = 'ubuntu'
+    env.user = 'ubuntu'
+    env.data_files = '/mnt/biodata'
+    env.galaxy_base = env.data_files + '/galaxy'
+    env.shell = "/bin/bash -l -c"
 
-def _add_defaults():
-    """Defaults from fabricrc.txt file; loaded if not specified at commandline.
+def local_server():
+    """Example setup for a local (non-cloud) server.
     """
-    if not env.has_key("distribution"):
-        config_file = os.path.join(env.config_dir, "fabricrc.txt")
-        if os.path.exists(config_file):
-            env.update(load_settings(config_file))
+    env.user = 'jingwenw'
+    env.data_files = '/glob/jingwenw/ref/'
+    env.galaxy_base = os.path.join(env.data_files, 'web')
+    env.shell = "/bin/bash -l -i -c"
 
 # -- Configuration for genomes to download and prepare
 
@@ -72,10 +60,17 @@ class UCSCGenome(_DownloadHelper):
         return self._name
 
     def download(self, seq_dir):
-        zipped_file = None
+        for zipped_file in ["chromFa.tar.gz", "%s.fa.gz" % self._name,
+                            "chromFa.zip"]:
+            if not self._exists(zipped_file, seq_dir):
+                with settings(warn_only=True):
+                    result = run("wget %s/%s" % (self._url, zipped_file))
+                if not result.failed:
+                    break
+            else:
+                break
         genome_file = "%s.fa" % self._name
         if not self._exists(genome_file, seq_dir):
-            zipped_file = self._download_zip(seq_dir)
             if zipped_file.endswith(".tar.gz"):
                 run("tar -xzpf %s" % zipped_file)
             elif zipped_file.endswith(".zip"):
@@ -102,18 +97,6 @@ class UCSCGenome(_DownloadHelper):
             run("rm -f *.fa")
             run("mv %s %s" % (tmp_file, genome_file))
         return genome_file, [zipped_file]
-
-    def _download_zip(self, seq_dir):
-        for zipped_file in ["chromFa.tar.gz", "%s.fa.gz" % self._name,
-                            "chromFa.zip"]:
-            if not self._exists(zipped_file, seq_dir):
-                with settings(warn_only=True):
-                    result = run("wget %s/%s" % (self._url, zipped_file))
-                if not result.failed:
-                    break
-            else:
-                break
-        return zipped_file
 
 class NCBIRest(_DownloadHelper):
     """Retrieve files using the TogoWS REST server pointed at NCBI.
@@ -143,10 +126,8 @@ class NCBIRest(_DownloadHelper):
 class EnsemblGenome(_DownloadHelper):
     """Retrieve genome FASTA files from Ensembl.
 
-    ftp://ftp.ensemblgenomes.org/pub/plants/release-3/fasta/
-    arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR9.55.dna.toplevel.fa.gz
-    ftp://ftp.ensembl.org/pub/release-56/fasta/
-    caenorhabditis_elegans/dna/Caenorhabditis_elegans.WS200.56.dna.toplevel.fa.gz
+    ftp://ftp.ensemblgenomes.org/pub/plants/release-3/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR9.55.dna.toplevel.fa.gz
+    ftp://ftp.ensembl.org/pub/release-56/fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WS200.56.dna.toplevel.fa.gz
     """
     def __init__(self, ensembl_section, release_number, release2, organism,
             name, convert_to_ucsc=False):
@@ -199,6 +180,8 @@ genomes = [
            ("Athaliana", "araTha_tair9", EnsemblGenome("plants", "6", "",
                "Arabidopsis_thaliana", "TAIR9")),
            ("Dmelanogaster", "dm3", UCSCGenome("dm3")),
+           #("Dmelanogaster", "BDGP5.13", EnsemblGenome("metazoa", "4", "55",
+           #    "Drosophila_melanogaster", "BDGP5.13", convert_to_ucsc=True),
            ("Celegans", "WS210", EnsemblGenome("standard", "60", "60",
                "Caenorhabditis_elegans", "WS210")),
            ("Mtuberculosis_H37Rv", "mycoTube_H37RV", NCBIRest("mycoTube_H37RV",
@@ -215,47 +198,19 @@ genomes = [
            ("Tguttata_Zebra_finch", "taeGut1", UCSCGenome("taeGut1")),
           ]
 
-genomes = [
-           ("phiX174", "phix", NCBIRest("phix", ["NC_001422.1"])),
-           ("Hsapiens", "hg19", UCSCGenome("hg19")),
-          ]
-
 lift_over_genomes = [g.ucsc_name() for (_, _, g) in genomes if g.ucsc_name()]
+#lift_over_genomes = ['hg18', 'hg19', 'mm9', 'xenTro2', 'rn4']
 
 # -- Fabric instructions
 
 def install_data():
     """Main entry point for installing useful biological data.
     """
-    _check_version()
-    setup_environment()
+    #amazon_ec2()
+    local_server()
     _data_uniref()
     _data_ngs_genomes()
     _data_liftover()
-
-def install_data_s3():
-    """Install data using pre-existing genomes present on Amazon s3.
-    """
-    _check_version()
-    setup_environment()
-    _download_genomes()
-
-def upload_s3():
-    """Upload prepared genome files by identifier to Amazon s3 buckets.
-    """
-    if boto is None:
-        raise ImportError("boto must be installed to upload to Amazon s3")
-    if env.host != "localhost" and not env.host.startswith(socket.gethostname()):
-        raise ValueError("Need to run S3 upload on a local machine")
-    _check_version()
-    setup_environment()
-    _data_ngs_genomes()
-    _upload_genomes()
-
-def _check_version():
-    version = env.version
-    if int(version.split(".")[0]) < 1:
-        raise NotImplementedError("Please install fabric version 1 or better")
 
 # == Decorators and context managers
 
@@ -300,37 +255,32 @@ def _data_ngs_genomes():
             seq_dir = 'seq'
             ref_file, base_zips = manager.download(seq_dir)
             ref_file = _move_seq_files(ref_file, base_zips, seq_dir)
-        _index_to_galaxy(cur_dir, ref_file, genome)
-
-def _index_to_galaxy(work_dir, ref_file, gid):
-    """Index sequence files and update associated Galaxy loc files.
-    """
-    with cd(work_dir):
-        sam_index = _index_sam(ref_file)
-        bwa_index = _index_bwa(ref_file)
-        bowtie_index = _index_bowtie(ref_file)
-        maq_index = _index_maq(ref_file)
-        _index_novoalign(ref_file)
-        twobit_index = _index_twobit(ref_file)
-        _index_eland(ref_file)
-        # other indexers not supported by default
-        if False:
-            bfast_index = _index_bfast(ref_file)
-            arachne_index = _index_arachne(ref_file)
-    for ref_index_file, cur_index, prefix, new_style in [
-            ("sam_fa_indices.loc", sam_index, "index", False),
-            ("alignseq.loc", twobit_index, "seq", False),
-            ("twobit.loc", twobit_index, "", False),
-            ("bowtie_indices.loc", bowtie_index, "", True),
-            ("bwa_index.loc", bwa_index, "", True),
-            ]:
-        if cur_index:
-            str_parts = [gid, os.path.join(work_dir, cur_index)]
-            if new_style:
-                str_parts = [gid, gid] + str_parts
-            if prefix:
-                str_parts.insert(0, prefix)
-            _update_loc_file(ref_index_file, str_parts)
+            bwa_index = _index_bwa(ref_file)
+            bowtie_index = _index_bowtie(ref_file)
+            maq_index = _index_maq(ref_file)
+            _index_novoalign(ref_file)
+            twobit_index = _index_twobit(ref_file)
+            _index_eland(ref_file)
+            # other indexers not supported by default
+            if False:
+                bfast_index = _index_bfast(ref_file)
+                arachne_index = _index_arachne(ref_file)
+            with cd(seq_dir):
+                sam_index = _index_sam(ref_file)
+        for ref_index_file, cur_index, prefix, new_style in [
+                ("sam_fa_indices.loc", sam_index, "index", False),
+                ("alignseq.loc", twobit_index, "seq", False),
+                ("twobit.loc", twobit_index, "", False),
+                ("bowtie_indices.loc", bowtie_index, "", True),
+                ("bwa_index.loc", bwa_index, "", True),
+                ]:
+            if cur_index:
+                str_parts = [genome, os.path.join(cur_dir, cur_index)]
+                if new_style:
+                    str_parts = [genome, genome] + str_parts
+                if prefix:
+                    str_parts.insert(0, prefix)
+                _update_loc_file(ref_index_file, str_parts)
 
 def _clean_genome_directory():
     """Remove any existing sequence information in the current directory.
@@ -359,49 +309,61 @@ def _update_loc_file(ref_file, line_parts):
         if not exists(tools_dir):
             conf_file = "tool_data_table_conf.xml"
             run("mkdir -p %s" % tools_dir)
-            put(os.path.join("installed_files", conf_file),
-                os.path.join(env.galaxy_base, conf_file))
+            put(os.path.join("/bubo/home/h15/jingwenw/private/galaxy/bcbb/ec2/biolinux/installed_files", conf_file),os.path.join(env.galaxy_base, conf_file))
         add_str = "\t".join(line_parts)
         with cd(tools_dir):
             if not exists(ref_file):
                 run("touch %s" % ref_file)
-            if not contains(ref_file, add_str):
-                append(ref_file, add_str)
+            if not contains(add_str, ref_file):
+                append(add_str, ref_file)
 
 @_if_installed("faToTwoBit")
 def _index_twobit(ref_file):
     """Index reference files using 2bit for random access.
     """
+    print("index_twobit")
     dir_name = "ucsc"
     ref_base = os.path.splitext(os.path.split(ref_file)[-1])[0]
     out_file = "%s.2bit" % ref_base
     if not exists(dir_name):
+	print("mkdir %s" % dir_name)
         run("mkdir %s" % dir_name)
     with cd(dir_name):
         if not exists(out_file):
+	    print("faToTwoBit %s %s" % (os.path.join(os.pardir, ref_file),
+                out_file))
             run("faToTwoBit %s %s" % (os.path.join(os.pardir, ref_file),
                 out_file))
     return os.path.join(dir_name, out_file)
 
 def _index_bowtie(ref_file):
+    print("index_bowtie")
     dir_name = "bowtie"
     ref_base = os.path.splitext(os.path.split(ref_file)[-1])[0]
     if not exists(dir_name):
-        run("mkdir %s" % dir_name)
+        print("mkdir %s" % dir_name)
+	run("mkdir %s" % dir_name)
         with cd(dir_name):
+            print("bowtie-build -f %s %s" % (
+                os.path.join(os.pardir, ref_file),
+                ref_base))
             run("bowtie-build -f %s %s" % (
                 os.path.join(os.pardir, ref_file),
                 ref_base))
     return os.path.join(dir_name, ref_base)
 
 def _index_bwa(ref_file):
+    print("index_bwa")
     dir_name = "bwa"
     local_ref = os.path.split(ref_file)[-1]
     if not exists(dir_name):
+	print("mkdir %s" % dir_name)
         run("mkdir %s" % dir_name)
         with cd(dir_name):
+	    print("ln -s %s" % os.path.join(os.pardir, ref_file))
             run("ln -s %s" % os.path.join(os.pardir, ref_file))
             with settings(warn_only=True):
+		print("bwa index -a bwtsw %s" % local_ref)
                 result = run("bwa index -a bwtsw %s" % local_ref)
             # work around a bug in bwa indexing for small files
             if result.failed:
@@ -410,13 +372,18 @@ def _index_bwa(ref_file):
     return os.path.join(dir_name, local_ref)
 
 def _index_maq(ref_file):
+    print("index_maq")
     dir_name = "maq"
     local_ref = os.path.split(ref_file)[-1]
     binary_out = "%s.bfa" % os.path.splitext(local_ref)[0]
     if not exists(dir_name):
+	print("mkdir %s" % dir_name)
         run("mkdir %s" % dir_name)
         with cd(dir_name):
+	    print("ln -s %s" % os.path.join(os.pardir, ref_file))
             run("ln -s %s" % os.path.join(os.pardir, ref_file))
+	    print("maq fasta2bfa %s %s" % (local_ref,
+                binary_out))
             run("maq fasta2bfa %s %s" % (local_ref,
                 binary_out))
     return os.path.join(dir_name, binary_out)
@@ -430,10 +397,10 @@ def _index_novoalign(ref_file):
         run("mkdir %s" % dir_name)
         with cd(dir_name):
             run("novoindex %s %s" % (index_name, ref_file))
-    _index_novoalign_cs(ref_file, index_name, dir_name)
+    _index_novoalign_cs(ref_file, dir_name)
 
 @_if_installed("novoalignCS")
-def _index_novoalign_cs(ref_file, index_name, dir_name):
+def _index_novoalign_cs(ref_file, dir_name):
     color_dir = os.path.join(dir_name, "colorspace")
     ref_file = os.path.join(os.pardir, ref_file)
     if not exists(color_dir):
@@ -442,10 +409,9 @@ def _index_novoalign_cs(ref_file, index_name, dir_name):
             run("novoindex -c %s %s" % (index_name, ref_file))
 
 def _index_sam(ref_file):
-    (ref_dir, local_file) = os.path.split(ref_file)
-    with cd(ref_dir):
-        if not exists("%s.fai" % local_file):
-            run("samtools faidx %s" % local_file)
+    (_, local_file) = os.path.split(ref_file)
+    if not exists("%s.fai" % local_file):
+        run("samtools faidx %s" % local_file)
     return ref_file
 
 @_if_installed("MakeLookupTable")
@@ -481,20 +447,21 @@ def _index_eland(ref_file):
         run("mkdir %s" % dir_name)
         num_refs = run("grep '^>' %s | wc -l" % ref_file)
         # For a lot of reference sequences, Eland needs them in 1 file
+	print("Num_refs:",num_refs)
         if int(num_refs) > 239:
             run("squashGenome %s %s" % (dir_name, ref_file))
         # For large reference sequences, squash fails and need them split up
         else:
             tmp_dir = "tmp_seqparts"
             run("mkdir %s" % tmp_dir)
-            run("seqretsplit -sequence %s -osdirectory2 %s -outseq ." %
-                    (ref_file, tmp_dir))
+            run("seqretsplit -sequence %s -osdirectory2 %s -outseq ." %(ref_file, tmp_dir))
             with cd(tmp_dir):
                 result = run("ls *.fasta")
                 result = result.split("\n")
+		result.pop()
             seq_files = [os.path.join(tmp_dir, f) for f in result]
-            run("squashGenome %s %s" % (dir_name, " ".join(seq_files)))
-            run("rm -rf %s" % tmp_dir)
+	    run("squashGenome %s %s" % (os.path.join(dir_name), " ".join(seq_files)))
+#            run("rm -rf %s" % tmp_dir)
             # Eland can only handle up to 24 reference files in a directory
             # If we have more, remove any with *random* in the name to get
             # below. This sucks, but seemingly no way around it because
@@ -509,100 +476,6 @@ def _index_eland(ref_file):
                     if int(new_count) // 2 > 24:
                         with settings(warn_only=True):
                             run("rm -f chrm*")
-
-# -- Genome upload and download to Amazon s3 buckets
-
-def _download_genomes():
-    """Download a group of genomes from Amazon s3 bucket.
-    """
-    genome_dir = os.path.join(env.data_files, "genomes")
-    for (orgname, gid, _) in genomes:
-        org_dir = os.path.join(genome_dir, orgname)
-        if not exists(org_dir):
-            run('mkdir -p %s' % org_dir)
-        with cd(org_dir):
-            if not exists(gid):
-                url = "https://s3.amazonaws.com/biodata/genomes/%s.tar.gz" % gid
-                run("wget %s" % url)
-                run("tar -xzvpf %s" % os.path.basename(url))
-                run("rm -f %s" % os.path.basename(url))
-        gid_dir = os.path.join(org_dir, gid)
-        ref_file = os.path.join(gid_dir, "seq", "%s.fa" % gid)
-        assert exists(ref_file), ref_file
-        _index_to_galaxy(gid_dir, ref_file, gid)
-
-def _upload_genomes():
-    """Upload our configured genomes to Amazon s3 bucket.
-    """
-    conn = boto.connect_s3()
-    bucket = conn.create_bucket("biodata")
-    genome_dir = os.path.join(env.data_files, "genomes")
-    for (orgname, gid, _) in genomes:
-        cur_dir = os.path.join(genome_dir, orgname, gid)
-        _clean_directory(cur_dir, gid)
-        tarball = _tar_directory(cur_dir)
-        _upload_to_s3(tarball, bucket)
-
-def _upload_to_s3(tarball, bucket):
-    """Upload the genome tarball to s3.
-    """
-    s3_key_name = os.path.join("genomes", os.path.basename(tarball))
-    if not bucket.get_key(s3_key_name):
-        gb_size = int(run("du -sm %s" % tarball).split()[0]) / 1000.0
-        print "Uploading %s %.1fGb" % (s3_key_name, gb_size)
-        if gb_size < 4.9:
-            s3_key = bucket.new_key(s3_key_name)
-            s3_key.set_contents_from_filename(tarball, reduced_redundancy=True)
-            s3_key.set_acl("public-read")
-        else:
-            _large_file_upload(bucket, s3_key_name, tarball)
-            s3_key = bucket.get_key(s3_key_name)
-            s3_key.set_acl("public-read")
-
-def _large_file_upload(bucket, s3_key_name, tarball):
-    """Upload large files using Amazon's multipart upload functionality.
-    """
-    def split_file(in_file):
-        prefix = os.path.join(os.path.dirname(in_file),
-                              "S3PART%s" % (os.path.basename(s3_key_name).split(".")[0]))
-        if not exists("%saa" % prefix):
-            run("split -b250m %s %s" % (in_file, prefix))
-        return sorted(glob.glob("%s*" % prefix))
-
-    mp = bucket.initiate_multipart_upload(s3_key_name)
-    for i, part in enumerate(split_file(tarball)):
-        print "Transferring", part
-        with open(part) as t_handle:
-            mp.upload_part_from_file(t_handle, i+1)
-        os.remove(part)
-    mp.complete_upload()
-
-def _tar_directory(dir):
-    """Create a tarball of the directory.
-    """
-    base_dir, tar_dir = os.path.split(dir)
-    tarball = os.path.join(base_dir, "%s.tar.gz" % tar_dir)
-    if not exists(tarball):
-        with cd(base_dir):
-            run("tar -czvpf %s %s" % (os.path.basename(tarball), tar_dir))
-    return tarball
-
-def _clean_directory(dir, gid):
-    """Clean duplicate files from directories before tar and upload.
-    """
-    # get rid of softlinks
-    bowtie_ln = os.path.join(dir, "bowtie", "%s.fa" % gid)
-    maq_ln = os.path.join(dir, "maq", "%s.fa" % gid)
-    for to_remove in [bowtie_ln, maq_ln]:
-        if exists(to_remove):
-            run("rm -f %s" % to_remove)
-    # remove any downloaded original sequence files
-    remove_exts = ["*.gz", "*.zip"]
-    with cd(os.path.join(dir, "seq")):
-        for rext in remove_exts:
-            fnames = run("find . -name '%s'" % rext)
-            for fname in (f.strip() for f in fnames.split("\n") if f.strip()):
-                run("rm -f %s" % fname)
 
 # == Liftover files
 
@@ -673,7 +546,6 @@ def _index_blast_db(work_dir, base_file, db_type):
             (exists("%s.%s" % (db_name, ext)) for ext in type_to_ext[db_type])):
             run("makeblastdb -in %s -dbtype %s -out %s" %
                     (base_file, db_type, db_name))
-
 
 # == Not used -- takes up too much space and time to index
 
